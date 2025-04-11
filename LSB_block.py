1955d50f8e1932b89fc0568824c2408d0ae2c99a
tamagochka <tamagochka@gmail.com>
2025-04-11 21:51:41 +0300

    added help for lsb pseudorandom permutation feature and stub for lsb block embedding

---
+++ LSB_block.py
@@ -0,0 +1,22 @@
+
+
+
+def LSB_block_embedding(
+        cover_file_path: str,
+        stego_file_path: str,
+        message_file_path: str
+    ):
+    """
+    Блочное погружение в НЗБ для большей стойкости к изменениям покрывающего объекта
+
+    Parameters
+    ----------
+        cover_file_path: str
+            путь к покрывающему объекту
+        stego_file_path: str
+            путь к стеганограмме
+        message_file_path: str
+            путь к файлу вложения
+    """
+
+    pass
